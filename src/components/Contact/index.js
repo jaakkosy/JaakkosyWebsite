7b740c02982a48bd112bbf205bc2885125871d91
Jaakko Syrjämäki <jaakkosyrjamaki92@hotmail.com>
2022-07-18 12:35:43 +0300

    first commit

---
+++ src/components/Contact/index.js
@@ -0,0 +1,116 @@
+import './index.scss'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { useState, useEffect, useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+
+
+const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
+    
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+    const sendEmail = (e) => {
+      e.preventDefault()
+
+      emailjs
+        .sendForm(
+          'gmail',
+          'template_7ceuptn',
+          refForm.current,
+          'L_q4nN5CC6YQG16V3'
+        )
+        .then(
+          () => {
+            alert('Message succesfully sent!')
+            window.location.reload(false)
+          },
+          () => {
+            alert('Failed to send the message, please try again!')
+          }
+        )
+    }
+
+    return (
+    <>
+        <div className='container contact-page'>
+            <div className='text-zone'>'
+                <h1>
+                    <AnimatedLetters
+                    letterClass={letterClass}
+                    strArray={['C','o','n','t','a','c','t',' ','m','e']}
+                    idx={15}
+                    />
+                </h1>
+                <p>
+                     I am interested in freelance opportunities - especially ambitious or 
+                     large projects. However, if you have other request or question, 
+                     don't hesitate to contact me using below form either.
+                </p>
+                <div className="contact-form">
+                  <form ref={refForm} onSubmit={sendEmail}>
+                    <ul>
+                      <li className="half">
+                        <input type="text" name="name" placeholder="Name" required/>
+                      </li>
+                      <li className="half">
+                        <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        required
+                        />
+                      </li>
+                      <li>
+                        <input 
+                        placeholder="Subject" 
+                        type="text" 
+                        name="subject" 
+                        required
+                        />
+                      </li>
+                      <li>
+                        <textarea 
+                        placeholder="Message" 
+                        name="message" 
+                        required
+                        ></textarea>
+                      </li>
+                      <li>
+                        <input type="submit" className='flat-button' value="SEND" />
+                      </li>
+                    </ul>
+                  </form>
+                </div>
+                </div>
+        <div className='info-map'>
+                Syrjämäki Jaakko,
+                <br />
+                Finland,
+                <br />
+                Mustarastaantie 4, 39160 <br/>
+                Ylöjärvi 
+                <br />
+                <span>jaakkosyrjamaki92@hotmail.com</span>
+                </div>
+                <div className='map-wrap'>
+                  <MapContainer center={[61.57543386187513, 23.50956881675213]} zoom={13}>
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    <Marker position={[61.57543386187513, 23.50956881675213]}>
+                      <Popup>Asun täällä, tule käymään!</Popup>
+                    </Marker>
+                  </MapContainer>
+                  </div>
+                  </div>
+        <Loader type="pacman" />
+    </>
+    )
+}
+
+export default Contact
